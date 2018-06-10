35d499ccdd7b8b0a731390650dd1364b8c41bcc7
Andrew Belt <andrewpbelt@gmail.com>
2018-06-10 04:44:04 -0400

    Switch to AUMIDIEffectBase

--- AU/BridgeAU.cpp
+++ AU/BridgeAU.cpp
@@ -1,14 +1,14 @@
 
-#include "MusicDeviceBase.h"
+#include "AUMIDIEffectBase.h"
 
 
 #include "../common/client.cpp"
 
 
-struct BridgeAU : public MusicDeviceBase {
+struct BridgeAU : public AUMIDIEffectBase {
 	BridgeClient *client;
 
-	BridgeAU(AudioUnit component) : MusicDeviceBase(component, BRIDGE_INPUTS, BRIDGE_OUTPUTS, 1) {
+	BridgeAU(AudioUnit component) : AUMIDIEffectBase(component) {
 		CreateElements();
 		Globals()->UseIndexedParameters(1 + BRIDGE_NUM_PARAMS);
 		client = new BridgeClient();
@@ -18,21 +18,13 @@ struct BridgeAU : public MusicDeviceBase {
 		delete client;
 	}
 
-	bool CanScheduleParameters() const override {
-		return false;
-	}
-
-	bool StreamFormatWritable(AudioUnitScope scope, AudioUnitElement element) override {
-		return !IsInitialized();
+	bool SupportsTail() override {
+		return true;
 	}
 
 	Float64 GetTailTime() override {
 		// 1 year, essentially infinite
 		return (60.0 * 60.0 * 24.0 * 365.0);
-	}
-
-	bool SupportsTail() override {
-		return true;
 	}
 
 	ComponentResult GetParameterValueStrings(AudioUnitScope inScope, AudioUnitParameterID inParameterID, CFArrayRef *outStrings) override {
@@ -60,97 +52,98 @@ struct BridgeAU : public MusicDeviceBase {
 			if (inParameterID == 0) {
 				FillInParameterName(outParameterInfo, CFSTR("Port"), false);
 				outParameterInfo.unit = kAudioUnitParameterUnit_Indexed;
-				outParameterInfo.minValue = 0.0;
+				outParameterInfo.minValue = 0.f;
 				outParameterInfo.maxValue = (float) (BRIDGE_NUM_PARAMS - 1);
-				outParameterInfo.defaultValue = 0.0;
+				outParameterInfo.defaultValue = 0.f;
 				return noErr;
 			}
 			else if (1 <= inParameterID && inParameterID < BRIDGE_NUM_PARAMS + 1) {
 				CFStringRef name = CFStringCreateWithFormat(NULL, NULL, CFSTR("CC %d"), (int) (inParameterID - 1));
 				FillInParameterName(outParameterInfo, name, false);
 				outParameterInfo.unit = kAudioUnitParameterUnit_Generic;
-				outParameterInfo.minValue = 0.0;
-				outParameterInfo.maxValue = 10.0;
-				outParameterInfo.defaultValue = 0.0;
+				outParameterInfo.minValue = 0.f;
+				outParameterInfo.maxValue = 10.f;
+				outParameterInfo.defaultValue = 0.f;
 				return noErr;
 			}
 		}
 		return kAudioUnitErr_InvalidParameter;
 	}
 
-	/*
-		OSStatus ProcessBufferLists(AudioUnitRenderActionFlags &ioActionFlags, const AudioBufferList &inBuffer, AudioBufferList &outBuffer, UInt32 inFramesToProcess) override {
-			// Push parameters
-			int port = (int) Globals()->GetParameter(0);
-			client->setPort(port);
-			for (int i = 0; i < BRIDGE_NUM_PARAMS; i++) {
-				float param = Globals()->GetParameter(i + 1) / 10.f;
-				client->setParam(i, param);
+	OSStatus GetParameter( AudioUnitParameterID inID, AudioUnitScope inScope, AudioUnitElement inElement, AudioUnitParameterValue &outValue) override {
+		if (inScope == kAudioUnitScope_Global) {
+			if (inID == 0) {
+				outValue = (float) client->getPort();
+				return noErr;
 			}
-			client->setSampleRate((int) GetOutput(0)->GetStreamFormat().mSampleRate);
-
-			// TODO Check that the stream is Float32, add better error handling.
-			float input[BRIDGE_INPUTS * inFramesToProcess];
-			float output[BRIDGE_OUTPUTS * inFramesToProcess];
-			memset(input, 0, sizeof(input));
-			memset(output, 0, sizeof(output));
-			// Interleave input
-			for (int c = 0; c < (int) inBuffer.mNumberBuffers; c++) {
-				const float *buffer = (const float*) inBuffer.mBuffers[c].mData;
-				for (int i = 0; i < (int) inFramesToProcess; i++) {
-					input[BRIDGE_INPUTS * i + c] = buffer[i];
-				}
+			else if (1 <= inID && inID < BRIDGE_NUM_PARAMS + 1) {
+				outValue = client->getParam(inID - 1);
+				return noErr;
 			}
-			// Process audio
-			client->processStream(input, output, inFramesToProcess);
-			// Deinterleave output
-			for (int c = 0; c < (int) outBuffer.mNumberBuffers; c++) {
-				float *buffer = (float*) outBuffer.mBuffers[c].mData;
-				for (int i = 0; i < (int) inFramesToProcess; i++) {
-					buffer[i] = output[BRIDGE_OUTPUTS * i + c];
-				}
-			}
-			return noErr;
 		}
-	*/
+		return kAudioUnitErr_InvalidParameter;
+	}
 
-	OSStatus Render(AudioUnitRenderActionFlags &ioActionFlags, const AudioTimeStamp& inTimeStamp, UInt32 nFrames) override {
-		// printf(".");
-		// AUInputElement *inputBus = GetInput(0);
-		// AudioBufferList &inputBufferList = inputBus->GetBufferList();
-		// AudioBuffer &inputBuffer = inputBufferList.mBuffers[0];
-		// float *input = (float*) inputBuffer.mData;
-		// printf("input buffers %d\n", inputBufferList.mNumberBuffers);
+	OSStatus SetParameter(AudioUnitParameterID inID, AudioUnitScope inScope, AudioUnitElement inElement, AudioUnitParameterValue inValue, UInt32 inBufferOffsetInFrames) override {
+		if (inScope == kAudioUnitScope_Global) {
+			if (inID == 0) {
+				client->setPort((int) roundf(inValue));
+				return noErr;
+			}
+			else if (1 <= inID && inID < BRIDGE_NUM_PARAMS + 1) {
+				client->setParam(inID - 1, inValue);
+				return noErr;
+			}
+		}
+		return kAudioUnitErr_InvalidParameter;
+	}
+
+	OSStatus ProcessBufferLists(AudioUnitRenderActionFlags &ioActionFlags, const AudioBufferList &inBuffer, AudioBufferList &outBuffer, UInt32 inFramesToProcess) override {
 		double beat;
 		double tempo;
 		if (!CallHostBeatAndTempo(&beat, &tempo)) {
 			printf("%f %f\n", beat, tempo);
 		}
 
-		AUOutputElement *outputBus = GetOutput(0);
-		outputBus->PrepareBuffer(nFrames);
-		AudioBufferList &outputBufferList = outputBus->GetBufferList();
-		printf("output buffers %d\n", outputBufferList.mNumberBuffers);
+		// Set sample rate
+		client->setSampleRate((int) GetOutput(0)->GetStreamFormat().mSampleRate);
 
-		for (int i = 0; i < outputBufferList.mNumberBuffers; i++) {
-			AudioBuffer &outputBuffer = outputBufferList.mBuffers[i];
-			float *output = (float*) outputBuffer.mData;
-			printf("\t%d channels %d\n", i, outputBuffer.mNumberChannels);
-			for (int j = 0; j < nFrames; j++) {
-				float r = (float) rand() / RAND_MAX;
-				for (int c = 0; c < outputBuffer.mNumberChannels; c++) {
-					output[j * outputBuffer.mNumberChannels + c] = (2.f * r - 1.f) * 0.01f;
-				}
+		// TODO Check that the stream is Float32, add better error handling.
+		float input[BRIDGE_INPUTS * inFramesToProcess];
+		float output[BRIDGE_OUTPUTS * inFramesToProcess];
+		memset(input, 0, sizeof(input));
+		memset(output, 0, sizeof(output));
+		// Interleave input
+		for (int c = 0; c < (int) inBuffer.mNumberBuffers; c++) {
+			const float *buffer = (const float*) inBuffer.mBuffers[c].mData;
+			for (int i = 0; i < (int) inFramesToProcess; i++) {
+				input[BRIDGE_INPUTS * i + c] = buffer[i];
 			}
 		}
+		// Process audio
+		client->processStream(input, output, inFramesToProcess);
+		// Deinterleave output
+		for (int c = 0; c < (int) outBuffer.mNumberBuffers; c++) {
+			float *buffer = (float*) outBuffer.mBuffers[c].mData;
+			for (int i = 0; i < (int) inFramesToProcess; i++) {
+				float r = (float) rand() / RAND_MAX;
+				buffer[i] = output[BRIDGE_OUTPUTS * i + c] + (r * 2.f - 1.f);
+			}
+		}
+		printf("%d\n", inFramesToProcess);
 		return noErr;
 	}
 
 	OSStatus MIDIEvent(UInt32 inStatus, UInt32 inData1, UInt32 inData2, UInt32 inOffsetSampleFrame) override {
-		printf("MIDIEvent: %02x %02x %02x at %d\n", inStatus, inData1, inData2, inOffsetSampleFrame);
+		MidiMessage msg;
+		msg.cmd = inStatus;
+		msg.data1 = inData1;
+		msg.data2 = inData2;
+		client->pushMidi(msg);
+		// log("%02x %02x %02x", msg.cmd, msg.data1, msg.data2);		
 		return noErr;
 	}
 };
 
 
-AUDIOCOMPONENT_ENTRY(AUMusicDeviceFactory, BridgeAU)
+AUDIOCOMPONENT_ENTRY(AUMIDIEffectFactory, BridgeAU)
